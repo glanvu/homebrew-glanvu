ad82baec054ff4f3d7252703700d89a62983ce8a
Juan Garcia Longaron <juan.mobilife@gmail.com>
2026-06-20 10:08:20 +0000

    glanvu 0.5.3: update formula

--- Formula/glanvu.rb
+++ Formula/glanvu.rb
@@ -4,22 +4,22 @@ class Glanvu < Formula
   license "Apache-2.0"
 
   on_linux do
-    url "https://github.com/glanvu/glanvu/releases/download/v0.5.0/glanvu-0.5.0-linux-x86_64.tar.gz"
-    sha256 "e9a462f2dd735f3e6034d40022a03e5af9ed92e2f0198d3047e3dab3ecd9f626"
-    version "0.5.0"
+    url "https://github.com/glanvu/glanvu/releases/download/v0.5.3/glanvu-0.5.3-linux-x86_64.tar.gz"
+    sha256 "21396c517602c1599bb69aa6d875fa72b96afacaa2c9efe3c82f6fe615631235"
+    version "0.5.3"
   end
 
   on_macos do
     on_arm do
-      url "https://github.com/glanvu/glanvu/releases/download/v0.5.0/Glanvu-0.5.0-macos-arm64.zip"
-      sha256 "51a8586eeac520ba97f02fe565bf037a42fd77c3a86d469a53bb7b410fa97f65"
-      version "0.5.0"
+      url "https://github.com/glanvu/glanvu/releases/download/v0.5.3/Glanvu-0.5.3-macos-arm64.zip"
+      sha256 "e18bd9cc414b4aca915cf9dd583f5a5d0f692b35817e29123b96d9e98e615d04"
+      version "0.5.3"
     end
 
     on_intel do
-      url "https://github.com/glanvu/glanvu/releases/download/v0.5.0/Glanvu-0.5.0-macos-x86_64.zip"
-      sha256 "2057242093d1dbda92824fb37e982ff8d797a7cbce286f502fe1b07b18577353"
-      version "0.5.0"
+      url "https://github.com/glanvu/glanvu/releases/download/v0.5.3/Glanvu-0.5.3-macos-x86_64.zip"
+      sha256 "fdf888a36d74db1dc5c9502b5e2e19138a54c5cb6432b9214347647f7f0e26bf"
+      version "0.5.3"
     end
   end
 
