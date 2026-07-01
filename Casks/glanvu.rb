c2fc7660983b18bd9d492c0f6f5c8ca5cdf72d5d
Juan Garcia Longaron <juan.mobilife@gmail.com>
2026-07-01 20:45:53 +0000

    glanvu 0.7.0: update cask

--- Casks/glanvu.rb
+++ Casks/glanvu.rb
@@ -1,14 +1,14 @@
 cask "glanvu" do
   # Update version + both sha256 values on each release (use scripts/bump-packaging.sh).
-  version "0.6.1"
+  version "0.7.0"
 
   on_arm do
-    sha256 "f680a3802f3ef102b007bd075f8266c2853149782984a981e5089aaba3209cfe"
+    sha256 "d15f534d6a915bb4098892d1b2df2f2ca27aa89185321216a6525aee632d535a"
     url "https://github.com/glanvu/glanvu/releases/download/v#{version}/Glanvu-#{version}-macos-arm64.zip"
   end
 
   on_intel do
-    sha256 "1f62a010acb3ef36a8f5f0ca6cf51753c4c7bf7dbd7bd0d807121133c53b6f56"
+    sha256 "70a8cf1b2c7eadef642def95be6f639115a2961cdb9f230db9d5c6279b04aba4"
     url "https://github.com/glanvu/glanvu/releases/download/v#{version}/Glanvu-#{version}-macos-x86_64.zip"
   end
 
